c0e180ba7fcf73b72acd1acc04eaea80fd96d38f
unknown <lutfinova6@gmail.com>
2026-05-01 08:25:50 +0700

    fix: remove automatic password changing bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,22 +28,9 @@ class AppServiceProvider extends ServiceProvider
             config([
                 'session.secure' => true,
                 'session.same_site' => 'lax',
-                'session.driver' => 'file',
+                'session.driver' => 'cookie',
                 'session.domain' => null,
             ]);
-
-            // Cek dan buat user admin otomatis jika belum ada (Fallback)
-            try {
-                \App\Models\User::updateOrCreate(
-                    ['email' => 'admin@smk.sch.id'],
-                    [
-                        'name' => 'Administrator',
-                        'password' => \Illuminate\Support\Facades\Hash::make('password')
-                    ]
-                );
-            } catch (\Exception $e) {
-                // Abaikan jika database belum siap
-            }
         }
     }
 }
