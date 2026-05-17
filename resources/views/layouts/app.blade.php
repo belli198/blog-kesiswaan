ae242c50b393b0938cc24c7bada9de4e3a50668b
unknown <lutfinova6@gmail.com>
2026-05-17 14:00:23 +0700

    Menambahkan favicon logo SMK di tab browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
         $siteName = \App\Models\Setting::where('key', 'site_name')->first()?->value ?? 'SMK Negeri 1 Adiwerna';
     @endphp
     <title>@yield('title') - {{ $siteName }}</title>
+    <link rel="icon" href="{{ asset('images/logo-smk.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('images/logo-smk.png') }}" type="image/png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
